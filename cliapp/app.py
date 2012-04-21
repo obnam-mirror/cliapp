4707e62c0d49238106dbe19bcfb90758a840fc76
Lars Wirzenius <liw@liw.fi>
2012-04-21 22:16:38 +0100

    Handle SystemExit with string values instead of integers
    If we raise the exception with a string value, the interpreter
    prints it out, but we want to print it out ourselves (and log it).

--- cliapp/app.py
+++ cliapp/app.py
@@ -172,7 +172,9 @@ class Application(object):
             stderr.write('ERROR: %s\n' % str(e))
             sys.exit(1)
         except SystemExit, e:
-            sys.exit(e.code)
+            logging.fatal(str(e))
+            sys.stderr.write('ERROR: %s\n' % str(e))
+            sys.exit(e.code if type(e.code) == int else 1)
         except KeyboardInterrupt, e:
             sys.exit(255)
         except IOError, e: # pragma: no cover
