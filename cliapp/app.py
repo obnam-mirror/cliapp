4cae8a1f1f307ba0a0ee22bb2aeedffeffc83bd3
Lars Wirzenius <liw@liw.fi>
2012-07-07 17:29:12 +0100

    Fix SystemExit error message printing

--- cliapp/app.py
+++ cliapp/app.py
@@ -174,6 +174,8 @@ class Application(object):
             stderr.write('ERROR: %s\n' % str(e))
             sys.exit(1)
         except SystemExit, e:
+            if e.code is not None and type(e.code) != int:
+                stderr.write('ERROR: %s\n' % str(e))
             sys.exit(e.code if type(e.code) == int else 1)
         except KeyboardInterrupt, e:
             sys.exit(255)
