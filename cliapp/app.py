9fd44074ec8b6ac4aee6c153029a06ec46cc903a
Lars Wirzenius <liw@liw.fi>
2012-10-09 15:50:07 +0100

    Handle process name setting IOErrors gracefully

--- cliapp/app.py
+++ cliapp/app.py
@@ -142,10 +142,13 @@ class Application(object):
         
         return ''.join(x.upper() if x in ok else '_' for x in basename)
 
-    def _set_process_name(self):
+    def _set_process_name(self): # pragma: no cover
         if platform.system() == 'Linux':
-            with open("/proc/%d/comm" % (os.getpid()), "wb", 0) as fh:
-                fh.write(self.settings.progname[:15])
+            try:
+                with open('/proc/self/comm', 'w', 0) as f:
+                    f.write(self.settings.progname[:15])
+            except IOError, e:
+                logging.warning(str(e))
 
     def _run(self, args=None, stderr=sys.stderr, log=logging.critical):
         try:
