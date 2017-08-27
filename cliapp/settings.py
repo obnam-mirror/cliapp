31ef5ba0bd4ea94d0efdbfb52683eb41ecba750a
Lars Wirzenius <liw@liw.fi>
2017-08-27 10:11:34 +0100

    Fix: disable unicode_literals from the future
    This should fix obnam.

--- cliapp/settings.py
+++ cliapp/settings.py
@@ -14,7 +14,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function  # unicode_literals
 
 try:
     from configparser import ConfigParser
